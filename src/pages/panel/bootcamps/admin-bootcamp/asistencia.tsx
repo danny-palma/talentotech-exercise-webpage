61ddbfa8946a8e109e73f29e78050f80810a5512
Daniel Palma <danielpalma2003@hotmail.com>
2024-06-26 06:45:51 -0500

    chore: updated code

--- src/pages/panel/bootcamps/admin-bootcamp/asistencia.tsx
+++ src/pages/panel/bootcamps/admin-bootcamp/asistencia.tsx
@@ -1,17 +1,55 @@
-//import React from 'react'
-
 import { useContext } from "react";
 
 import { pageContext } from "../../../../contexts/panel-page-indexer";
 
 function Asistencia() {
-  const { SetPageState } = useContext(pageContext);
-  SetPageState("asistencia");
-  return (
-    <div>
-      <h1>Control de Asistencias</h1>
-    </div>
-  )
+    const { SetPageState } = useContext(pageContext);
+    SetPageState("asistencia");
+    return (
+        <div>
+            <h1>Control de Asistencias</h1>
+            <div>
+                <div className="container-xl">
+                    <table className="table-striped table-hover table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Edad</th>
+                                <th scope="col">Ciudad</th>
+                                <th scope="col">Clase1</th>
+                                <th scope="col">Clase2</th>
+                                <th scope="col">Clase3</th>
+                                <th scope="col">Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Camila Mikan</td>
+                                <td>25</td>
+                                <td>Bogotá</td>
+                                <td>Asistio</td>
+                                <td>Asistio</td>
+                                <td>No Asistio</td>
+                                <td>89%</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Daniel Palma</td>
+                                <td>25</td>
+                                <td>Bogotá</td>
+                                <td>Asistio</td>
+                                <td>Asistio</td>
+                                <td>No Asistio</td>
+                                <td>79%</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Asistencia;
