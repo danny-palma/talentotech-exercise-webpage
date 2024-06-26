45094b2ed85560bb04beb25b241982d639a8bfa8
Bladito77 <edblatoco@gmail.com>
2024-06-25 20:17:55 -0500

    chore: added admin pages

---
+++ src/pages/panel/bootcamps/admin-bootcamp/admin.tsx
@@ -0,0 +1,13 @@
+
+/**
+ *  Asociar a bootcamp, eliminar y actualizar, notas y asociaciones al bootcamp
+ *  NO EDITA NINGUNA INFORMACION BASICA DEL USUARIO (COOREO, NOMBRES, CONTRASEÑAS ... ETC)
+ *  EDITAR INFORMACION RELACIONADA AL BOOTCAMP (SESIONES, VINCULOS EXTERNOS, FOROS, MENTORIAS)
+ */
+function AdminBootcamp() {
+  return (
+    <div>Admin</div>
+  )
+}
+
+export default AdminBootcamp
