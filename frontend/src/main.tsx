93a78358294ed498aee602e69b59c68bcbd201bc
Daniel Palma <danielpalma2003@hotmail.com>
2024-06-29 19:14:42 -0500

    chore: Updated to react 18

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-
-import ReactDOM from "react-dom";
-
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ThemeContextProvider } from "./contexts/change-theme";
 
-ReactDOM.render(
+const Container = document.getElementById("root");
+const root = createRoot(Container!);
+
+root.render(
     <React.StrictMode>
         <ThemeContextProvider>
             <App />
         </ThemeContextProvider>
     </React.StrictMode>,
-    document.getElementById("root"),
 );
