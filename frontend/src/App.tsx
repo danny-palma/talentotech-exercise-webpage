0344bccf59c66b8591a8827cd98bb34a18e6b872
Daniel Palma <danielpalma2003@hotmail.com>
2024-06-29 21:57:23 -0500

    Feat: add user context provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import { Routes, Route, HashRouter } from "react-router-dom";
-
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { PageContextProvider } from "./contexts/panel-page-indexer";
+import { UserProvider } from "./contexts/user";
 import Login from "./pages/login/login";
 import Main from "./pages/main/main";
 import PanelIndex from "./pages/panel";
@@ -20,36 +19,44 @@ import PanelMain from "./pages/panel/main/main";
 import Marketplace from "./pages/panel/marketplace/marketplace";
 import UserManual from "./pages/panel/user-manual/user-manual";
 
-
 function App() {
     return (
-        <PageContextProvider>
-            <HashRouter>
-                <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/panel" element={<PanelIndex />}>
-
-                        <Route path="" element={<PanelMain />} />
-                        <Route path="admin" element={<AdminMain />} />
-                        <Route path="bootcamps" element={<Bootcamps />} />
-                        <Route
-                            path="bootcamps/:id" //* < -- /panel/bootcamps/"prueba" (id)
-                            element={<BootcampDetails />}
-                        />
-                        <Route path="bootcamps/:id/admin" //* <-- /panel/bootcamp/"pueba"/admin
-                            element={<AdminBootcamp />} />
-                        <Route path="califica" element={<Gradres />} />
-                        <Route path="asistencia" element={<Asistencia />} />
-                        <Route path="hackatons" element={<Hackathons />} />
-                        <Route path="jobs" element={<Jobs />} />
-                        <Route path="marketplace" element={<Marketplace />} />
-                        <Route path="user-manual" element={<UserManual />} />
-                        <Route path="faq" element={<Faq />} />
-                    </Route>
-                </Routes>
-            </HashRouter>
-        </PageContextProvider>
+        <UserProvider>
+            <PageContextProvider>
+                <HashRouter>
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/panel" element={<PanelIndex />}>
+                            <Route path="" element={<PanelMain />} />
+                            <Route path="admin" element={<AdminMain />} />
+                            <Route path="bootcamps" element={<Bootcamps />} />
+                            <Route
+                                path="bootcamps/:id" //* < -- /panel/bootcamps/"prueba" (id)
+                                element={<BootcampDetails />}
+                            />
+                            <Route
+                                path="bootcamps/:id/admin" //* <-- /panel/bootcamp/"pueba"/admin
+                                element={<AdminBootcamp />}
+                            />
+                            <Route path="califica" element={<Gradres />} />
+                            <Route path="asistencia" element={<Asistencia />} />
+                            <Route path="hackatons" element={<Hackathons />} />
+                            <Route path="jobs" element={<Jobs />} />
+                            <Route
+                                path="marketplace"
+                                element={<Marketplace />}
+                            />
+                            <Route
+                                path="user-manual"
+                                element={<UserManual />}
+                            />
+                            <Route path="faq" element={<Faq />} />
+                        </Route>
+                    </Routes>
+                </HashRouter>
+            </PageContextProvider>
+        </UserProvider>
     );
 }
 
