8f36c19231cbaff31221efc3a9b41e31ec6b9494
Daniel Palma <danielpalma2003@hotmail.com>
2024-07-03 00:12:18 -0500

    chore: update code to use connection uri

--- backend/src/sql/sql-conection.ts
+++ backend/src/sql/sql-conection.ts
@@ -3,11 +3,8 @@ import { createPool } from "mysql2/promise";
 // Crear el pool de conexiones
 const pool = createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
   typeCast: true,
+  uri: process.env.DB_URI
 });
 
 // Manejar evento de conexión exitosa
